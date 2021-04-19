05d96ef47f0b02f53e574125ea0038939993a640
katsuyaw <kw@kmail.me>
2021-04-18 18:19:39 -0700

    Fix format

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import Navbar from './components/Navbar';
+import Home from './pages';
+
+
+
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+
+    </>
   );
 }
 
