d5bdc27d533f880c55d1b1a9bb667731be90bac6
katsuyaw <kw@kmail.me>
2021-04-19 16:04:34 -0700

    Add desktop logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,10 +18,12 @@ const Home = () => {
 
             <div className="grid  grid-cols-1  sm:grid-cols-2 gap-10 m-10 h-full ">
                 {/* first card */}
-                <div className="card">
-                    <div className="">
-                        All Work in One Place, Anywhere3
+                <div className="relative grid grid-rows-2 grid-flow-col hover:bg-gray-300 bg-gray-200 rounded-3xl font-bold py-20  justify-items-center">
+                    <div className="flex justify-center">
+                        <svg className="w-20 h-20" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
+                        <svg className="w-20 h-20" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                     </div>
+                    <p className="absolute bottom-10">All Work in One Place, Anywhere</p>
                 </div>
                 <div className="card">Auto Optimized Inbox</div>
                 <div className="card">Privacy Built-in</div>
