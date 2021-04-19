22383e9fffeaa465a552845600dd4e599ec91272
katsuyaw <kw@kmail.me>
2021-04-18 19:03:32 -0700

    Fix Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({ toggle }) => {
   return (
-    <nav className="flex justify-between items-center h-16 relative ">
-      <Link to='/' className='pl-8' role="navigation">
+    <nav className="flex justify-between items-center h-16 relative font-bold p-16">
+      <Link to='/' className='text-3xl pl-6' role="navigation">
         Efficiently.app
-            </Link>
-      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
+      </Link>
+      <div className="cursor-pointer md:hidden" onClick={toggle}>
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
       </div>
-      <div className="pr-8 md:block hidden">
-        <Link className="p-4" to="/">Home</Link>
+      <div className="md:block hidden">
+        <a className="p-4" href="https://twitter.com/doefficiently">@DoEfficiently</a>
       </div>
+
     </nav>
   )
 }
