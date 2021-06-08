82da39843d0def740423660efabe31d635ceb6b6
katsuyaw <kw@kmail.me>
2021-06-07 19:36:57 -0700

    Minor change

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className="flex justify-start items-center h-20 pl-15 from-gray-50 text-black">
+            <p className="ml-14">&copy; 2021 KN Project</p>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,6 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-            <div className="grid  grid-cols-1  sm:grid-cols-2 gap-10 m-10 h-full ">
-                {/* first card */}
-                <div className="relative grid grid-rows-2 grid-flow-col hover:bg-gray-300 bg-gray-200 rounded-3xl font-bold py-20  justify-items-center">
-                    <div className="flex justify-center">
-                        <div className="flex flex-wrap">
-                            <svg className="w-20 h-20 blue-pulse px-1 flex-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
-                            <svg className="w-20 h-20 blue-pulse px-1 flex-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                            <svg className="w-20 h-20 blue-pulse px-1 flex-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                        </div>
-                    </div>
-                    <p className="absolute bottom-10 mx-5"></p>
-                </div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-            </div>
-
 
         </>
     )
