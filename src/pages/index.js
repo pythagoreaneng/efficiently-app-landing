6f8666b02a2fe0e504246e1b5b240b0e445993e1
katsuyaw <kw@kmail.me>
2021-06-19 19:06:14 -0700

    Add card colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,25 @@ const Home = () => {
         <>
             <div className="grid justify-items-stretch">
                 <Card 
+                    style={{backgroundColor: "#EAEC8C"}}
                     title='Plan'
                     body='Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor. Rem ipsum dolor sit amet,  consectetur adipiscing elit.'
                 />
                 <Card 
+                    style={{backgroundColor: "#447DD2"}}
                     title='Work'
                     body='Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor. Rem ipsum dolor sit amet,  consectetur adipiscing elit.'
                 />
-                
+                <Card 
+                    style={{backgroundColor: "#447DD2"}}
+                    title='Think'
+                    body='Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor. Rem ipsum dolor sit amet,  consectetur adipiscing elit.'
+                />
+                <Card 
+                    style={{backgroundColor: "#FFD8D7"}}
+                    title='Play'
+                    body='Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor. Rem ipsum dolor sit amet,  consectetur adipiscing elit.'
+                />
             </div>
         </>
     )
