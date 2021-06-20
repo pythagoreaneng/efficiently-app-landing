24df814bc72aa32dbe94a91d2e582d79510ec3f7
katsuyaw <kw@kmail.me>
2021-06-19 19:15:12 -0700

    Add page logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({ toggle }) => {
   return (
-    <nav className="flex justify-between items-center h-16 relative font-bold p-10">
+    <nav className="flex justify-between items-center relative p-10">
       <Link to='/' className='text-base ' role="navigation">
-        Efficiently
+        <h1 className="text-4xl font-medium text-blue-400 pb-3">Efficiently</h1>
+        <h2 className="font-thin">Upgrade Your Efficiency.</h2>
       </Link>
-      <div className="cursor-pointer md:hidden" onClick={toggle}>
+      <div className="cursor-pointer hidden" onClick={toggle}>
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
       </div>
-      <div className="md:block hidden">
-        <a className="" href="https://twitter.com/doefficiently">@doefficiently</a>
+      <div className="">
+        <a className="font-bold" href="https://twitter.com/doefficiently">@doefficiently</a>
       </div>
 
     </nav>
