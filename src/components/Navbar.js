54048fb1f29b5a6fd6b84cdf96702d5ff74b8391
katsuyaw <kw@kmail.me>
2021-06-29 11:19:10 -0700

    Fix title and favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ toggle }) => {
   return (
     <nav className="flex justify-between items-center relative p-10">
-      <Link to='/' className='text-base ' role="navigation">
-        <img src=''/>
-        <h1 className="text-4xl font-semibold text-blue-400 pb-3">Efficiently</h1>
-        <h2 className="font-thin">Upgrade Your Efficiency.</h2>
+      <Link to="/" className="text-base " role="navigation">
+        <div>
+          <h1 className="text-4xl font-semibold text-blue-400 ">Efficiently</h1>
+          <h2 className="font-thin">Upgrade Your Efficiency.</h2>
+        </div>
       </Link>
       <div className="cursor-pointer hidden" onClick={toggle}>
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
       </div>
       <div className="invisible sm:visible">
-        <a className="font-bold rounded-full p-4 bg-blue-400 shadow-sm hover:shadow-lg text-white" href="https://twitter.com/doefficiently">@doefficiently</a>
+        <a
+          className="font-bold rounded-full p-4 bg-blue-400 shadow-sm hover:shadow-lg text-white"
+          href="https://twitter.com/doefficiently"
+        >
+          @doefficiently
+        </a>
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
