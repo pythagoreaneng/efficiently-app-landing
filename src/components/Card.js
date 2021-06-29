57982560511401350a707e600cdc91936ed866ba
katsuyaw <kw@kmail.me>
2021-06-29 11:25:47 -0700

    Fix formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,34 @@
-import React from 'react'
+import React from "react";
 
 function Card(props) {
-    return (
-        <div className="p-10 rounded-xl shadow-2xl bg-gray-100 text-white" style={props.style}>
+  return (
+    <div
+      className="p-10 rounded-xl shadow-2xl bg-gray-100 text-white"
+      style={props.style}
+    >
+      <div className="p-2 text-3xl md:text-5xl font-bold">
+        <h3>
+          {props.title} <span className="text-blue-400">Efficiently</span>
+        </h3>
+      </div>
 
-            <div className="p-2 text-3xl md:text-5xl font-bold">
-                <h3>{props.title} <span className="text-blue-400">Efficiently</span></h3>
-            </div>
-
-            <div className="sm:grid grid-cols-2">
-
-                {/* left container */}
-                <div className="">
-                    
-                    <div className="p-5 text-gray-700">
-                        <p>{props.body}</p>
-                    </div>
-
-                </div>
-
-                {/* right container */}
-                <div className="">
-
-                    <div className="image-container">
-                        <img src={props.imageUrl} alt="" />
-                    </div>
-
-                </div>
-            </div>
-            
-            
+      <div className="sm:grid grid-cols-2">
+        {/* left container */}
+        <div className="">
+          <div className="p-5 text-gray-700">
+            <p>{props.body}</p>
+          </div>
         </div>
-    )
+
+        {/* right container */}
+        <div className="">
+          <div className="image-container">
+            <img src={props.imageUrl} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
