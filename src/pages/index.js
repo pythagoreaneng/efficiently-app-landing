8d63fc7309f02671e7c70a29858044e65216cb5a
Nasser <nasserbenyed@gmail.com>
2021-07-04 01:49:54 -0700

    test form 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,40 +48,28 @@ const Home = () => {
 
       <div className="my-10 rounded-xl shadow-2xl bg-gray-100" id="newsletter">
         <div className="py-28 text-center">
-          <p className="font-bold text-3xl md:text-5xl">Join the Waitlist</p>
-          <p className="mt-2 mb-10 font-thin text-xs">
-            Subscribe to our newsletter to get early access
-          </p>
+      
+            <p className="font-bold text-3xl md:text-5xl">Join the Waitlist</p>
+            <p className="mt-2 mb-10 font-thin text-xs">
+              Subscribe to our newsletter to get early access
+            </p>
 
           <form name="contact" method="POST" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Role:{" "}
-                <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select>
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message"></textarea>
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="rounded-md text-center p-1"
+              placeholder="email@example.com"
+              require
+            />
+            <button
+              type="submit"
+              className="m-2 px-2 py-1 text-sm text-white bg-blue-400 rounded-md"
+            >
+              Join
+            </button>
           </form>
         </div>
       </div>
