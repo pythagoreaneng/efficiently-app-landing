21f507b1ad5f3ded0e97a5f1bc90d093ee4f6874
Nasser <nasserbenyed@gmail.com>
2021-07-04 01:37:13 -0700

    test form 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,15 +48,13 @@ const Home = () => {
 
       <div className="my-10 rounded-xl shadow-2xl bg-gray-100" id="newsletter">
         <div className="py-28 text-center">
-          <label htmlFor="email">
+      
             <p className="font-bold text-3xl md:text-5xl">Join the Waitlist</p>
             <p className="mt-2 mb-10 font-thin text-xs">
               Subscribe to our newsletter to get early access
             </p>
-          </label>
 
           <form name="contact" method="POST" data-netlify="true">
-            {/* test */}
             <input type="hidden" name="form-name" value="contact" />
             <input
               id="email"
