d567012a8e37d4490d3773879da2ef1258fe0c7f
katsuyaw <kw@kmail.me>
2021-07-06 10:42:38 -0700

    Fix spacing

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,32 +5,28 @@ const Card = (props) => {
   const content = props.content;
   return (
     <div
-      className="p-10 rounded-xl shadow-2xl bg-gray-100 text-white"
+      className="px-10 py-20 rounded-xl shadow-2xl text-white h-full"
       style={props.style}
     >
-      <div className="p-2 text-3xl md:text-5xl font-bold">
+      <div className="p-2 text-2xl md:text-4xl font-bold">
         <h3>
-          {props.title} <span className="text-blue-400">Efficiently</span>
+          <span className="text-white">{props.title}</span>{" "}
+          <span className="text-blue-400">Efficiently</span>
         </h3>
       </div>
 
       <div className="sm:grid grid-cols-2">
         {/* left container */}
         <div className="">
-          <div className="p-5 text-gray-600 font-normal">
+          <div className="px-3 py-20 text-gray-600">
             <p>{props.body}</p>
           </div>
         </div>
 
         {/* right container */}
         <div className="">
-          {image && (
-            <div className="flex justify-center">
-              <img src={props.image} alt="" />
-            </div>
-          )}
           {content && (
-            <div className="flex justify-center">{props.content}</div>
+            <div className="flex justify-center py-20">{props.content}</div>
           )}
         </div>
       </div>
