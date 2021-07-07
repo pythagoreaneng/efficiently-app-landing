4e63e1905b45e3b35ec437f2eea925a87cd5b608
katsuyaw <kw@kmail.me>
2021-07-07 14:27:48 -0700

    Improve spacing

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,32 +4,29 @@ const Card = (props) => {
   const image = props.image;
   const content = props.content;
   return (
-    <div
-      className="px-8 py-16 rounded-xl shadow-2xl text-white h-full"
-      style={props.style}
-    >
-      <div className=" text-3xl sm:text-5xl font-bold">
-        <h3>
-          <span>{props.title}</span>{" "}
-          <span className="efficiently-blue">Efficiently</span>
-        </h3>
-      </div>
-
-      <div className="grid xl:grid-cols-2 h-full">
-        {/* left container */}
-
-        <div className="px-3 py-20 text-gray-600 h-full">
-          <p>{props.body}</p>
+    <>
+      <div
+        className="px-8 py-16 rounded-xl shadow-2xl text-white h-full"
+        style={props.style}
+      >
+        <div className="text-3xl sm:text-5xl font-bold">
+          <h3>
+            <span>{props.title}</span>{" "}
+            <span className="efficiently-blue">Efficiently</span>
+          </h3>
         </div>
 
-        {/* right container */}
-        <div className="">
-          <div className="flex items-center justify-center py-8 h-full">
+        <div className="grid xl:grid-cols-2 h-full">
+          <div className="text-gray-600 h-full mt-10">
+            <p>{props.body}</p>
+          </div>
+
+          <div className="flex h-72 items-center justify-center">
             {props.content}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
