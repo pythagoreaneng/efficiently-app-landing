42d11d6057f7de19a6c4e640f09ccd5a9ee062f4
katsuyaw <kw@kmail.me>
2021-07-09 13:19:22 -0700

    Improve spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,26 @@ import Mockup from "../img/Mockup.svg";
 export default function Header() {
   return (
     <>
-      <div className="md:flex ">
-        <h1 className="m-auto">
-          <div className="grid grid-col">
-            <p className="flex justify-center font-bold text-4xl sm:text-7xl lg:text-8xl ">
+      <div className="md:flex md:flex-rows-2 h-full md:items-center ">
+        <div className="flex flex-1 justify-center">
+          <div className="flex flex-col md:items-start">
+            <p className="flex justify-center font-bold text-4xl sm:text-6xl lg:text-7xl ">
               Do&nbsp;
               <span className="efficiently-blue">Efficiently</span>
             </p>
             <p className="flex justify-center md:justify-start text-gray-500 pl-2">
               All-in-One Productivity App
             </p>
-          </div>
-
-          <div className="flex justify-center md:justify-start">
+            <div className="flex justify-center md:justify-start">
             <button className="bg-efficiently-blue rounded-3xl font-medium text-white p-3 my-8">
-              <a href="#newsletter">Early Access</a>
+              <a href="#newsletter">Join the Waitlist</a>
             </button>
           </div>
-        </h1>
-        <div className="flex justify-center md:m-auto">
+          </div>
+        </div>
+
+
+        <div className="flex flex-1 w-full justify-center">
           <img className="w-72" src={Mockup} />
         </div>
       </div>
