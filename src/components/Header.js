8aba7e792577c894c8e69d62b04b4fe43abf71bd
katsuyaw <kw@kmail.me>
2021-07-09 13:40:43 -0700

    Improve spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Mockup from "../img/Mockup.svg";
 export default function Header() {
   return (
     <>
-      <div className="md:flex md:flex-rows-2 h-full md:items-center ">
+      <div className="md:flex md:flex-rows-2 h-full md:items-center my-20">
         <div className="flex flex-1 justify-center">
           <div className="flex flex-col md:items-start">
             <p className="flex justify-center font-bold text-4xl sm:text-6xl lg:text-7xl ">
@@ -15,13 +15,12 @@ export default function Header() {
               All-in-One Productivity App
             </p>
             <div className="flex justify-center md:justify-start">
-            <button className="bg-efficiently-blue rounded-3xl font-medium text-white p-3 my-8">
-              <a href="#newsletter">Join the Waitlist</a>
-            </button>
-          </div>
+              <button className="bg-efficiently-blue rounded-3xl font-medium text-white p-3 my-8">
+                <a href="#newsletter">Join the Waitlist</a>
+              </button>
+            </div>
           </div>
         </div>
-
 
         <div className="flex flex-1 w-full justify-center">
           <img className="w-72" src={Mockup} />
