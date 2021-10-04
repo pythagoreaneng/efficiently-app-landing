89e320bef8f16589dee6ed6e332934dc98db75a8
katsuyawakabayashi <kw@kmail.me>
2021-10-03 21:11:20 -0700

    Edit team title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Twitter from "../img/Twitter.svg";
 function Footer() {
   return (
     <div className="flex justify-around items-center h-20 text-black">
-      <p>&copy; 2021 Pythagorean Software</p>
+      <p>&copy; 2021 Pythagorean Engineering</p>
       <a href="https://twitter.com/doefficiently" className="sm:hidden">
         <img className="h-7 w-auto" alt="twitter-icon" src={Twitter} />
       </a>
